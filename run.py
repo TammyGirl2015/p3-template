53c033aab91e5fe3a7ca5e568f2f2df378ea0288
TammyGirl2015 <mtamarie@yahoo.com>
2024-03-20 10:14:41 +0000

    add welcome and instructions

--- run.py
+++ run.py
@@ -1,26 +1,30 @@
 from random import randint
 
-"""
-Options for player to input
-"""
-
+# Options for player to input
 t = ["rock", "paper", "scissors"]
 
-"""
-Computer chooses random option
-"""
+# Function to display game instructions
+def game_instructions():
+    print("\nWelcome to Rock-Paper-Scissors.")
+    print("Type in rock, paper or scissors in lowercase. The Computer will randomly pick one of the three choices.\n")
+    print("Rock crushes Scissors")
+    print("Scissors cuts Paper")
+    print("Paper covers Rock\n")
+    print("The game is the best out of three and you can choose whether to continue playing or not. Good Luck!\n")
 
+# Display game instructions
+game_instructions()
+
+# Computer chooses random option
 computer = t[randint(0,2)]
 
 player = False
 
 while player == False:
-    """
-    While loop: sets the parameters for winning, losing and tying for the player and the computer. 
-    The code checks that the correct words, case sensitive, are input. 
-    Returns error message if input is anything other than Rock, Paper or Scissors.
-    """
-    player = input("rock, paper, scissors?")
+    # While loop: sets the parameters for winning, losing, and tying for the player and the computer.
+    # The code checks that the correct words, case sensitive, are input.
+    # Returns error message if input is anything other than Rock, Paper, or Scissors.
+    player = input("rock, paper, scissors? ").lower() 
     if player == computer:
         print("Tie!")
     elif player == "rock":
@@ -39,8 +43,6 @@ while player == False:
         else:
             print("You win!", player, "cut", computer)
     else:
-        print("That's not a valid play. Check your spelling!")
-    player = False
-    computer = t[randint(0,2)]
-    
-
+        print("That's not a valid play. Choose rock, paper or scissors.")
+    player = False 
+    computer = t[randint(0,2)] 
